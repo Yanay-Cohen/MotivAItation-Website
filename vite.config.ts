ed4bf8f024914a80d46a0ba9df53044d9d8915ce
Yanay <yanayco123@gmail.com>
2025-07-26 16:48:58 +0300

    Deploy or something

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Add this line:
+const ghPagesPath = '/MotivAItation-Website/'
 
 export default defineConfig({
+  base: ghPagesPath, // IMPORTANT for correct routing
   plugins: [react()],
-});
-
-
+})
